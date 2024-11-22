1e499da6f0448b1172bcf1fdd6a89347cbf94415
Raru8 <victor.gbr2023@gmail.com>
2024-11-21 22:56:50 -0300

    New adjustments to the route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import {AuthGuard} from './core/guard/auth.guard';
 import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '**', redirectTo: 'login'},
   {path: 'home', component:HomeComponent, canActivate: [AuthGuard]},
   {path: 'login', component: AuthComponent}
 ];
